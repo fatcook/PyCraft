ddfdaf582490628510675c888d1e0b2299f0df51
fatcook <601200860@qq.com>
2022-10-03 10:56:29 +0800

    游戏逻辑初始化，加载预制件方法

--- Assets/PyCraft/Scripts/BaseElemnt/Chunck/ChunckBase.cs
+++ Assets/PyCraft/Scripts/BaseElemnt/Chunck/ChunckBase.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 namespace PyCraft
 {
-    //[RequireComponent(typeof(MeshFilter))]
-    //[RequireComponent(typeof(MeshRenderer))]
-    //[RequireComponent(typeof(BoxCollider))]
     public class ChunckBase : MonoBehaviour
     {
+        /// <summary>
+        /// 构造方法
+        /// </summary>
         public ChunckBase()
         {
-            this.gameObject.AddComponent<MeshFilter>();
-            this.gameObject.AddComponent<MeshRenderer>();
-            this.gameObject.AddComponent<BoxCollider>();
+
         }
     }
 }
--- Assets/PyCraft/Scripts/Managers/GameMgr.cs
+++ Assets/PyCraft/Scripts/Managers/GameMgr.cs
@@ -1,15 +1,24 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UpUtility.Runtime;
 
 namespace PyCraft
 {
-    public class GameMgr
+    public class GameMgr : MonoSingleton<GameMgr>
     {
-        public void Init()
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public async void Init()
         {
-            var tmpChunck = new TeseChunck();
-
+            //加载预制件
+            var prefabTask = ResMgr.Instance().LoadPrefab().ContinueWith(() =>
+            {
+                Debug.Log("预制件加载成功!");
+            });
+            await UniTask.WhenAll(prefabTask);
         }
     }
 }
