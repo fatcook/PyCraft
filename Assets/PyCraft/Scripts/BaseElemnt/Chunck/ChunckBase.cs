f155a2bac2cff261d71cbc65959328664c4ff11f
fatcook <601200860@qq.com>
2022-10-04 12:04:56 +0800

    生成地形测试

--- Assets/PyCraft/Scripts/BaseElemnt/Chunck/ChunckBase.cs
+++ Assets/PyCraft/Scripts/BaseElemnt/Chunck/ChunckBase.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 namespace PyCraft
@@ -7,12 +9,99 @@ namespace PyCraft
     public class ChunckBase : MonoBehaviour
     {
         /// <summary>
+        /// 顶点列表
+        /// </summary>
+        private List<Vector3> vertices = new List<Vector3>();
+        /// <summary>
+        /// 顶点索引列表
+        /// </summary>
+        private List<int> triangleIndex = new List<int>();
+
+        /// <summary>
+        /// MeshCollider
+        /// </summary>
+        private MeshCollider meshCollider;
+        /// <summary>
+        /// MeshCollider
+        /// </summary>
+        private MeshFilter meshFilter;
+
+        /// <summary>
         /// 构造方法
         /// </summary>
         public ChunckBase()
         {
 
         }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Init()
+        {
+            meshCollider = GetComponent<MeshCollider>();
+            meshFilter = GetComponent<MeshFilter>();
+        }
+
+        /// <summary>
+        /// 添加立方体面
+        /// </summary>
+        /// <param name="type">立方体面类型</param>
+        public void AddPanel(PanelType type, int x, int y, int z)
+        {
+            var tmpBasePosList = GameMgr.Instance().BaseVerticesPosDic[type];
+            triangleIndex.Add(0 + vertices.Count);
+            triangleIndex.Add(1 + vertices.Count);
+            triangleIndex.Add(2 + vertices.Count);
+            triangleIndex.Add(2 + vertices.Count);
+            triangleIndex.Add(3 + vertices.Count);
+            triangleIndex.Add(0 + vertices.Count);
+            for (int i = 0; i < tmpBasePosList.Count; i++)
+            {
+                var tmpVertic = new Vector3(tmpBasePosList[i].x + x, tmpBasePosList[i].y + y, tmpBasePosList[i].z + z);
+                vertices.Add(tmpVertic);
+            }
+        }
+
+        /// <summary>
+        /// 渲染方块面
+        /// </summary>
+        public void RenderChunckPanel()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    for (int z = 0; z < 10; z++)
+                    {
+                        var mesh = new Mesh();
+                        mesh.name = "ChunckBase";
+                        //生成方块六个面
+                        foreach (PanelType type in Enum.GetValues(typeof(PanelType)))
+                            AddPanel(type, x, y, z);
+                        mesh.vertices = vertices.ToArray();
+                        mesh.triangles = triangleIndex.ToArray();
+                        mesh.RecalculateBounds();
+                        mesh.RecalculateNormals();
+                        meshCollider.sharedMesh = mesh;
+                        meshFilter.mesh = mesh;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 立方体面类型枚举
+    /// </summary>
+    public enum PanelType
+    {
+        Front,
+        Back,
+        Left,
+        Right,
+        Top,
+        Down
     }
 }
 
--- Assets/PyCraft/Scripts/Managers/GameMgr.cs
+++ Assets/PyCraft/Scripts/Managers/GameMgr.cs
@@ -9,6 +9,77 @@ namespace PyCraft
     public class GameMgr : MonoSingleton<GameMgr>
     {
         /// <summary>
+        /// 基础顶点坐标((0,0,0)开始顺时针到(0,1,0)共四个点)
+        /// </summary>
+        private Dictionary<PanelType, List<Vector3>> baseVerticesPosDic = new Dictionary<PanelType, List<Vector3>>();
+
+
+        /// <summary>
+        /// 基础顶点坐标((0,0,0)开始顺时针到(0,1,0)共四个点)
+        /// </summary>
+        public Dictionary<PanelType, List<Vector3>> BaseVerticesPosDic
+        {
+            get
+            {
+                if (baseVerticesPosDic == null)
+                    baseVerticesPosDic = new Dictionary<PanelType, List<Vector3>>();
+                else if (baseVerticesPosDic?.Count < 6)
+                {
+                    baseVerticesPosDic.Clear();
+                    if (!baseVerticesPosDic.ContainsKey(PanelType.Front))
+                    {
+                        baseVerticesPosDic.Add(PanelType.Front, new List<Vector3>());
+                        baseVerticesPosDic[PanelType.Front].Add(new Vector3(1, 0, 0));
+                        baseVerticesPosDic[PanelType.Front].Add(new Vector3(1, 1, 0));
+                        baseVerticesPosDic[PanelType.Front].Add(new Vector3(1, 1, 1));
+                        baseVerticesPosDic[PanelType.Front].Add(new Vector3(1, 0, 1));
+                    }
+                    if (!baseVerticesPosDic.ContainsKey(PanelType.Back))
+                    {
+                        baseVerticesPosDic.Add(PanelType.Back, new List<Vector3>());
+                        baseVerticesPosDic[PanelType.Back].Add(new Vector3(0, 0, 0));
+                        baseVerticesPosDic[PanelType.Back].Add(new Vector3(0, 0, 1));
+                        baseVerticesPosDic[PanelType.Back].Add(new Vector3(0, 1, 1));
+                        baseVerticesPosDic[PanelType.Back].Add(new Vector3(0, 1, 0));
+                    }
+                    if (!baseVerticesPosDic.ContainsKey(PanelType.Left))
+                    {
+                        baseVerticesPosDic.Add(PanelType.Left, new List<Vector3>());
+                        baseVerticesPosDic[PanelType.Left].Add(new Vector3(0, 0, 0));
+                        baseVerticesPosDic[PanelType.Left].Add(new Vector3(0, 1, 0));
+                        baseVerticesPosDic[PanelType.Left].Add(new Vector3(1, 1, 0));
+                        baseVerticesPosDic[PanelType.Left].Add(new Vector3(1, 0, 0));
+                    }
+                    if (!baseVerticesPosDic.ContainsKey(PanelType.Right))
+                    {
+                        baseVerticesPosDic.Add(PanelType.Right, new List<Vector3>());
+                        baseVerticesPosDic[PanelType.Right].Add(new Vector3(0, 0, 1));
+                        baseVerticesPosDic[PanelType.Right].Add(new Vector3(1, 0, 1));
+                        baseVerticesPosDic[PanelType.Right].Add(new Vector3(1, 1, 1));
+                        baseVerticesPosDic[PanelType.Right].Add(new Vector3(0, 1, 1));
+                    }
+                    if (!baseVerticesPosDic.ContainsKey(PanelType.Top))
+                    {
+                        baseVerticesPosDic.Add(PanelType.Top, new List<Vector3>());
+                        baseVerticesPosDic[PanelType.Top].Add(new Vector3(0, 1, 0));
+                        baseVerticesPosDic[PanelType.Top].Add(new Vector3(0, 1, 1));
+                        baseVerticesPosDic[PanelType.Top].Add(new Vector3(1, 1, 1));
+                        baseVerticesPosDic[PanelType.Top].Add(new Vector3(1, 1, 0));
+                    }
+                    if (!baseVerticesPosDic.ContainsKey(PanelType.Down))
+                    {
+                        baseVerticesPosDic.Add(PanelType.Down, new List<Vector3>());
+                        baseVerticesPosDic[PanelType.Down].Add(new Vector3(0, 0, 0));
+                        baseVerticesPosDic[PanelType.Down].Add(new Vector3(0, 0, 1));
+                        baseVerticesPosDic[PanelType.Down].Add(new Vector3(1, 0, 1));
+                        baseVerticesPosDic[PanelType.Down].Add(new Vector3(1, 0, 0));
+                    }
+                }
+                return baseVerticesPosDic;
+            }
+            set => baseVerticesPosDic = value;
+        }
+        /// <summary>
         /// 初始化
         /// </summary>
         public async void Init()
@@ -19,6 +90,7 @@ namespace PyCraft
                 Debug.Log("预制件加载成功!");
             });
             await UniTask.WhenAll(prefabTask);
+
         }
     }
 }
